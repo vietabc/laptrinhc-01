7aa8d26e0992259e4ddd89f9b7e2f382cde4a8fe
vietabc <tranquangjoey18@gmail.com>
2018-11-28 19:53:24 +0700

    ฉชช

---
+++ Untitled3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<math.h>
+using namespace std; 
+main() {
+	int a,b;
+	float x;
+	x=-b/a;
+	cin >> a >> b;
+    if (a==0) {
+    	if (b==0) cout <<"phuong trinh vo so nghiem ";
+    	else cout <<"phuong trinh vo nghiem";
+	}
+	else cout<<x;
+	
+	
+	 
+	
+}
